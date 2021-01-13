fd873a70ce611a9e46d32822a72b87c9d57cc201
Ajakaye <mailpaulade@gmail.com>
2021-01-13 13:57:14 +0100

    created signup and login html and also created userprofile model

---
+++ apps/toinkerprofile/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class ToinkerProfile(models.Model):
+	user = models.OneToOneField(User,on_delete=models.CASCADE)
+	follows =models.ManyToManyField('self',related_name='followed_by',symmetrical=False)#references itself ,symmetrical is set to false because if i follow you,you dont automatically follow me back
+
+User.toinkerprofile=property(lambda u: OinkerProfile.objects.get_or_create(user=u)[0])# when i signup i dont have to create an oinker profile
+
