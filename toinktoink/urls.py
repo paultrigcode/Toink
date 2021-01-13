1c6749dae71750757f0754da5f050ef609218f6d
Ajakaye <mailpaulade@gmail.com>
2021-01-13 14:17:52 +0100

    crreated signup view fxn, created signup and login html

--- toinktoink/urls.py
+++ toinktoink/urls.py
@@ -15,12 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from apps.core.views import frontpage
+from apps.core.views import frontpage,signup
 
 urlpatterns = [
 	#
 	#
 	path('',frontpage,name='frontpage'),
+	path('signup/',signup,name='signup'),
+
     
 	#
 	#Admin
