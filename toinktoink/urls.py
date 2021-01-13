7208cb000cab615557fbaf8da98e93c15da15e11
Ajakaye <mailpaulade@gmail.com>
2021-01-13 13:35:11 +0100

    added base.html,frontpage.html,setup bulmacss,crrated frontpage view and url

--- toinktoink/urls.py
+++ toinktoink/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from apps.core.views import frontpage
 
 urlpatterns = [
+	#
+	#
+	path('',frontpage,name='frontpage'),
+    
+	#
+	#Admin
     path('admin/', admin.site.urls),
 ]
+ 
