31f5fd963fc3665c2db4333e91b379b4946e288b
Ajakaye <mailpaulade@gmail.com>
2021-01-14 15:37:29 +0100

    added humanize to installed app to read the time from seconds,completed the submit and rettrieve endpoint for oink feeds

--- apps/feed/views.py
+++ apps/feed/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Oink
 
 # Create your views here.
 
 @login_required
 def feed(request):
-	return render(request,'feed/feed.html')
+	userids = [request.user.id]
+
+	for oinker in request.user.toinkerprofile.follows.all():
+		userids.append(oinker.user.id)
+
+	oinks = Oink.objects.filter(created_by_id__in=userids)
+	return render(request,'feed/feed.html',{'oinks':oinks})
--- apps/toinkerprofile/models.py
+++ apps/toinkerprofile/models.py
@@ -7,5 +7,5 @@ class ToinkerProfile(models.Model):
 	user = models.OneToOneField(User,on_delete=models.CASCADE)
 	follows =models.ManyToManyField('self',related_name='followed_by',symmetrical=False)#references itself ,symmetrical is set to false because if i follow you,you dont automatically follow me back
 
-User.toinkerprofile=property(lambda u: OinkerProfile.objects.get_or_create(user=u)[0])# when i signup i dont have to create an oinker profile
+User.toinkerprofile=property(lambda u: ToinkerProfile.objects.get_or_create(user=u)[0])# when i signup i dont have to create an oinker profile
 
